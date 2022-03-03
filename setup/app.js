0c74603e0f9a9095b6378cb846de699109e3021f
Sidharth Sundaram <sidharth.rama@gmail.com>
2022-03-03 09:13:56 +0530

    update counter project

--- setup/app.js
+++ setup/app.js
@@ -1,16 +1,37 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//set initial count
+let count = 0;
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+//select value & buttons
 
-btn.addEventListener("click", function(){
-    //getting random number b/w 0 - 3
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-});
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn")
 
-function getRandomNumber(){
-    return Math.floor(Math.random() * colors.length);
-}
+//we can run a for each loop on a nodeList
+
+btns.forEach(function(btn){    //we can give a parameter to call each and every item
+    btn.addEventListener('click', function(e){  //function(e)is an event object function to actively check which button is getting clicked on
+    const styles = e.currentTarget.classList; // to check what classList my button has. this will help in easily identifying, and then manipulating values on screen
+    if(styles.contains("decrease")){ //i'm looking for my decrease class here
+        count--;
+    }
+
+    else if(styles.contains("increase")){
+        count++;
+    }
+    else{
+        count = 0;
+    }
+    if(count > 0){
+        value.style.color = "green";
+    }
+
+    if(count < 0){
+        value.style.color = "red";
+    }
+
+    if(count === 0){
+        value.style.color = "#222";
+    }
+    value.textContent = count;
+    }) //we are looping over the list to basically call all the buttons. This particular code will tell you which exact button was pressed.
+})
